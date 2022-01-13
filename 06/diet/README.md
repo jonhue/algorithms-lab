31f80e9e02ca489deaf709251f901fc4ce7bf69b
Jonas Hübotter <jonas.huebotter@gmail.com>
2022-01-13 13:48:41 +0100

    diet

---
+++ 06/diet/README.md
@@ -0,0 +1,3 @@
+# Diet
+
+This is yet another simple problem to become familiarized with multi-variable linear programs in CGAL.
---
+++ 06/diet/main.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+#include <CGAL/QP_models.h>
+#include <CGAL/QP_functions.h>
+#include <CGAL/Gmpz.h>
+
+typedef int IT;
+typedef CGAL::Gmpq ET;
+
+// program and solution types
+typedef CGAL::Quadratic_program<IT> Program;
+typedef CGAL::Quadratic_program_solution<ET> Solution;
+
+void solve(const int n, const int m) {
+  std::vector<std::pair<int, int>> nutrients;
+  for (int i = 0; i < n; i++) {
+    int min, max; std::cin >> min >> max;
+    nutrients.push_back({min, max});
+  }
+
+  std::vector<int> prices;
+  std::vector<std::vector<int>> foods(m, std::vector<int>());
+  for (int j = 0; j < m; j++) {
+    int p; std::cin >> p;
+    prices.push_back(p);
+
+    for (int i = 0; i < n; i++) {
+      int C; std::cin >> C;
+      foods[j].push_back(C);
+    }
+  }
+
+  Program lp (CGAL::SMALLER, true, 0, false, 0);
+  int cnt = 0;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      lp.set_a(j, cnt, -foods[j][i]);
+      lp.set_a(j, cnt + 1, foods[j][i]);
+    }
+
+    auto [min, max] = nutrients[i];
+    lp.set_b(cnt, -min);
+    lp.set_b(cnt + 1, max);
+
+    cnt += 2;
+  }
+
+  for (int j = 0; j < m; j++) {
+    lp.set_c(j, prices[j]);
+  }
+
+  Solution s = CGAL::solve_linear_program(lp, ET());
+  if (s.is_infeasible()) {
+    std::cout << "No such diet." << std::endl;
+  } else {
+    int result = std::floor(CGAL::to_double(s.objective_value()));
+    std::cout << result << std::endl;
+  }
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+
+  int n, m; std::cin >> n >> m;
+  while (n > 0 && m > 0) {
+    solve(n, m);
+    std::cin >> n >> m;
+  }
+}
